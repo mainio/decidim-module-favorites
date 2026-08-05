beb20ab13d501edf1af556a5f8b6c907672d1807
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2026-08-05 13:38:02 +0300

    Upgrade to v.31

--- config/assets.rb
+++ config/assets.rb
@@ -2,5 +2,5 @@
 
 base_path = File.expand_path("..", __dir__)
 
-Decidim::Webpacker.register_path("#{base_path}/app/packs", prepend: true)
-Decidim::Webpacker.register_stylesheet_import("stylesheets/decidim/favorites/favorites")
+Decidim::Shakapacker.register_path("#{base_path}/app/packs", prepend: true)
+Decidim::Shakapacker.register_stylesheet_import("stylesheets/decidim/favorites/favorites")
